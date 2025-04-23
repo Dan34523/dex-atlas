756ca795203ccf74bf9a0bfeb03f0f212a93fd53
Dan34523 <danyal.durrani123@gmail.com>
2025-04-23 22:30:23 +0100

    use @for to generate clickable cards

--- src/app/features/landing-page/landing-page.component.ts
+++ src/app/features/landing-page/landing-page.component.ts
@@ -3,6 +3,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { RouterModule } from '@angular/router';
 import { ClickableCardComponent } from '../../shared/clickable-card/clickable-card.component';
+import { Feature } from './models/feature.model';
 
 @Component({
     selector: 'app-landing-page',
@@ -12,11 +13,24 @@ import { ClickableCardComponent } from '../../shared/clickable-card/clickable-ca
         MatButtonModule,
         MatButtonModule,
         MatCardModule,
-        ClickableCardComponent
+        ClickableCardComponent,
     ],
     templateUrl: './landing-page.component.html',
-    styleUrls: ['./landing-page.component.scss']
+    styleUrls: ['./landing-page.component.scss'],
 })
 export class LandingPageComponent {
     hover = false;
+
+    features: Feature[] = [
+        {
+            title: 'Team Planner',
+            description: 'Build your ultimate Pokémon team!',
+            link: '/team-planner',
+        },
+        {
+            title: 'Region Map',
+            description: 'Explore the Pokémon world!',
+            link: '/region-map',
+        },
+    ];
 }
--- src/app/shared/clickable-card/clickable-card.component.ts
+++ src/app/shared/clickable-card/clickable-card.component.ts
@@ -1,17 +1,19 @@
 import { Component, Input } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatRippleModule } from '@angular/material/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
     selector: 'app-clickable-card',
     standalone: true,
-    imports: [MatCardModule, MatRippleModule],
+    imports: [MatCardModule, MatRippleModule, RouterModule],
     templateUrl: './clickable-card.component.html',
     styleUrl: './clickable-card.component.scss',
 })
 export class ClickableCardComponent {
-    @Input() title: string = 'Team Builder';
-    @Input() description: string = 'Build your ultimate Pokémon team!';
+    @Input() title: string = 'Title';
+    @Input() description: string = 'Description';
+    @Input() link: string = '/';
 
     hover = false;
 }
